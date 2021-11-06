65ef604afbac8e59f1b2a5ff8ed44609c6dde8a1
cjy97 <857369174@qq.com>
2021-11-06 15:27:10 +0000

    1

--- finetune_cifar.py
+++ finetune_cifar.py
@@ -27,8 +27,8 @@ def prepare_data(dataset, dataset_path):
         trainset = torchvision.datasets.CIFAR100(root=dataset_path, train=True, download=True, transform=transform)
         testset = torchvision.datasets.CIFAR100(root=dataset_path, train=False, download=True, transform=transform)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=2)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
